96ad9d9a572c306aea31818991187edc6af61e6b
Millo Lailang <sunoy14dev@gmail.com>
2023-05-29 02:15:31 +0530

    basic server and client

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/status', (req, res) => {
+	res.send({
+		message: 'hello world!'
+	})
+})
+
+app.listen(process.env.PORT || 8081)
